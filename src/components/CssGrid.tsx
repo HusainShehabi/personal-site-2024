276fdc7c1bd04a244a304c8c6659b540afc98ed5
HusainShehabi <shehabi.hussain@gmail.com>
2024-05-11 17:20:54 +0300

    responsive

--- src/components/CssGrid.tsx
+++ src/components/CssGrid.tsx
@@ -1,18 +1,27 @@
 import GridItem from "./GridItem";
+import ExperienceCard from "./content/ExperienceCard";
+import FooterCard from "./content/FooterCard";
 import IntroductionCard from "./content/IntroductionCard";
+import SocialsCard from "./content/SocialsCard";
 
 type Props = {};
 
 export default function CssGrid({}: Props) {
   return (
-    <div className="grid grid-cols-5 gap-5 p-4  min-h-screen">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-5 p-4 min-h-screen">
       <GridItem className="col-span-3 row-span-4">
         <IntroductionCard />
       </GridItem>
-      <GridItem className="col-span-2 row-span-3">medium Item</GridItem>
-      <GridItem className="col-span-2 row-span-1">Small Item</GridItem>
-      <GridItem className="col-span-1 row-span-2">Tall Item</GridItem>
-      <GridItem className="col-span-4 row-span-2">Wide Item</GridItem>
+      <GridItem className="col-span-2 row-span-3">
+        <ExperienceCard />
+      </GridItem>
+      <GridItem className="col-span-2 row-span-1">
+        <SocialsCard />
+      </GridItem>
+      <GridItem className="col-span-2 row-span-2">
+        <FooterCard />
+      </GridItem>
+      <GridItem className="col-span-3 row-span-2">Wide Item</GridItem>
     </div>
   );
 }
