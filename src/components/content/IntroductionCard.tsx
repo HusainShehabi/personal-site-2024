625ad9e6176026cade3ef9733b56a51554314361
HusainShehabi <shehabi.hussain@gmail.com>
2024-05-11 17:21:52 +0300

    various cards added

--- src/components/content/IntroductionCard.tsx
+++ src/components/content/IntroductionCard.tsx
@@ -4,25 +4,31 @@ type Props = {};
 
 export default function IntroductionCard({}: Props) {
   return (
-    <div style={{ width: "100%", height: "100%" }}>
+    <div className="w-full h-full font-sans text-2xl text-gray-300">
       <div className="grid grid-cols-2 gap-4">
-        <div
-          style={{ border: "1px solid green" }}
-          className="col-span-2 row-span-3 "
-        >
-          fgh
+        <div className="col-span-2 row-span-3 font-sans font-semibold text-6xl text-gray-300 p-10 pb-0 pl-5">
+          Husain Shehabi
         </div>
-        <div style={{ border: "1px solid green" }}>01</div>
-        <div
-          style={{ border: "1px solid green" }}
-          className="col-span-2 row-span-3 font-sans font-extrabold text-6xl text-white"
-        >
-          Hello, I'm Husain
+        <div className="col-span-2 row-span-1 ml-5 mt-0">
+          FullStack Developer
         </div>
-        <div style={{ border: "1px solid green" }}>01</div>S
-        <div style={{ border: "1px solid green" }}>01</div>
-        <div style={{ border: "1px solid green" }}>01</div>
-        <div style={{ border: "1px solid green" }}>09</div>
+        <div className="col-span-2 row-span-12 text-base ml-5 mr-5">
+          <p>
+            I'm a <b>full-stack developer</b> with <b>3 years</b> of experience.
+            I enjoy building appealing and interactive sites & apps.
+          </p>
+        </div>
+
+        <div className="col-span-1 row-span-3 text-base ml-5 mr-5">
+          When I’m not at the computer, I’m usually rock climbing, reading,
+          hanging out with my wife and two cats, or running around Hyrule
+          searching for Korok seeds K o r o k s e e d s .
+        </div>
+        <div className="col-span-1 row-span-3 text-base ml-5 mr-5">
+          When I’m not at the computer, I’m usually rock climbing, reading,
+          hanging out with my wife and two cats, or running around Hyrule
+          searching for Korok seeds K o r o k s e e d s .
+        </div>
       </div>
     </div>
   );
---
+++ src/components/content/SocialsCard.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function s({}: Props) {
+  return <div>Github LinkedIn </div>;
+}
